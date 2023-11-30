5fc3e92758fd6f2539a38831f2277c90f582580d
Belen Molina <belen.molina.z@gmail.com>
2023-11-30 17:43:04 -0500

    listado cards total e individual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CardComponent } from './components/card/card.component';
+import { ListComponent } from './pages/list/list.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { HttpClientModule } from '@angular/common/http';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MarketNamePipe } from './pipes/market-name.pipe'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    ListComponent,
+    DetailComponent,
+    MarketNamePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    InfiniteScrollModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
